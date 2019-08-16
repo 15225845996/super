b439adc35606df2decf7be8507cdd97cdfac135a
zs <http://repo.eastonsoft.com/dashboard/projects>
2019-08-16 15:58:12 +0800

    正常画图保存

--- super-admin-service/src/main/java/com/zs/adminservice/controller/activiti/ActivitiController.java
+++ super-admin-service/src/main/java/com/zs/adminservice/controller/activiti/ActivitiController.java
@@ -3,6 +3,7 @@ package com.zs.adminservice.controller.activiti;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.zs.adminservice.service.IActivitiService;
 import org.activiti.bpmn.converter.BpmnXMLConverter;
 import org.activiti.bpmn.model.BpmnModel;
 import org.activiti.editor.constants.ModelDataJsonConstants;
@@ -10,6 +11,7 @@ import org.activiti.editor.language.json.converter.BpmnJsonConverter;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.Model;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,13 +19,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/models")
@@ -35,6 +40,8 @@ public class ActivitiController {
     private RepositoryService repositoryService;
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private IActivitiService activitiService;
 
     @RequestMapping("/modelList")
     public String modelList(org.springframework.ui.Model model,HttpServletRequest request){
@@ -128,10 +135,18 @@ public class ActivitiController {
     }
 
     @RequestMapping("/start/{id}")
-    public @ResponseBody String startProcess(@PathVariable("id") String id){
+    public @ResponseBody String startProcess(@PathVariable("id") String id,@RequestParam(name = "startUser",defaultValue = "员工") String startUser){
         try {
-
-
+            if(StringUtils.isNotBlank(id)){
+                Map<String,Object> map = new HashMap<>();
+                map.put("subUser",startUser);
+                ProcessInstance processInstance = activitiService.startProcess(id,map);
+                if(processInstance == null){
+                    return "流程启动失败！";
+                }
+            }else{
+                return "ID不能为空";
+            }
         }catch (Exception e){
             e.printStackTrace();
             return "流程启动失败！";
